b7a98b49a136fd13bca2ea5ed9236c2495f62a76
SeckMohameth <mohamethseck34@gmail.com>
2023-04-30 22:00:50 -0400

    Initial Commit

---
+++ vers-ii/vers_iiApp.swift
@@ -0,0 +1,17 @@
+//
+//  vers_iiApp.swift
+//  vers-ii
+//
+//  Created by Mohameth Seck on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct vers_iiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
